a73b66f609c569fa1ca679f35c28d37ff4aa50a2
naza <nazari_93@hotmail.com>
2021-03-10 14:26:32 +0100

    clase casa 10/03/2021

---
+++ MigracionTablesAzure/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace MigracionTablesAzure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmigrardatos = new System.Windows.Forms.Button();
+            this.lblmensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnmigrardatos
+            // 
+            this.btnmigrardatos.Location = new System.Drawing.Point(105, 29);
+            this.btnmigrardatos.Name = "btnmigrardatos";
+            this.btnmigrardatos.Size = new System.Drawing.Size(216, 57);
+            this.btnmigrardatos.TabIndex = 0;
+            this.btnmigrardatos.Text = "Migrar datos";
+            this.btnmigrardatos.UseVisualStyleBackColor = true;
+            // 
+            // lblmensaje
+            // 
+            this.lblmensaje.AutoSize = true;
+            this.lblmensaje.Location = new System.Drawing.Point(36, 126);
+            this.lblmensaje.Name = "lblmensaje";
+            this.lblmensaje.Size = new System.Drawing.Size(115, 25);
+            this.lblmensaje.TabIndex = 1;
+            this.lblmensaje.Text = "lblmensaje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 239);
+            this.Controls.Add(this.lblmensaje);
+            this.Controls.Add(this.btnmigrardatos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmigrardatos;
+        private System.Windows.Forms.Label lblmensaje;
+    }
+}
+
